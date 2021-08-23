0b90346518b1313cdaa38b48d0e7a481e5c55ac5
MARCOS JAMPIETRI <marcosjampietri@marcoss-macbook-pro.local>
2021-08-23 20:20:40 +0000

    fix breakpoint types

--- client/styles/breakpoints.ts
+++ client/styles/breakpoints.ts
@@ -1,18 +1,17 @@
 import { css } from "styled-components";
 
-const size = {
+interface MediaQueryProps {
+    [key: string]: number;
+}
+
+const size: MediaQueryProps = {
     small: 400,
     med: 960,
     large: 1140,
 };
 
-interface BreakProps {
-    acc: Object;
-    label: string;
-}
-
 export const below = Object.keys(size).reduce((acc: any, label: string) => {
-    acc[label] = (...args: any) => css`
+    acc[label] = (...args: [any]) => css`
         @media (max-width: ${size[label] / 16}em) {
             ${css(...args)}
         }
