234899527de03e0aff91ccde61e5d213333fe308
MARCOS JAMPIETRI <marcosjampietri@marcoss-macbook-pro.local>
2021-08-23 20:32:19 +0000

    rm single quote from title

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -32,7 +32,7 @@ const Layout = ({ children }: LayoutProps) => {
     return (
         <>
             <Head>
-                <title>MARCOS JAMPIETRI'S PORTFOLIO</title>
+                <title>MARCOS JAMPIETRI S PORTFOLIO</title>
                 <meta
                     name="viewport"
                     content="initial-scale=1.0, width=device-width"
